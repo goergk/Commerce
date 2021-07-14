da1135c7d3bf11ae561391f260d290104be3c616
goergk <grzegorzkaw124@gmail.com>
2021-07-14 11:57:43 +0200

    Models bugs fixed (no migrations yet)

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,18 +4,21 @@ from django.db.models.expressions import Value
 
 
 class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
     pass
 
     def __str__(self):
         return f"{self.username}"
 
 class Category(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
 
     def __str__(self):
         f"{self.name}"
 
 class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
     value = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
@@ -24,20 +27,22 @@ class Bid(models.Model):
         f"Last bid: {self.date} by {self.user}. Bid value: {self.value}$."
 
 class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
     topic = models.CharField(max_length=50)
     content = models.CharField(max_length=2500)
     date = models.DateTimeField()
-    commentator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="original poster")
+    commentator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="original_poster")
 
 class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.CharField(max_length=2500)
     image_url = models.CharField(max_length=500)
     creation_date = models.DateTimeField()
     closing_date = models.DateTimeField()
-    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="last bid")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product category")
-    creator =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="listing creator")
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="last_bid")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_category")
+    creator =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="listing_creator")
 
 
